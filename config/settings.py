792e0e354bb51e94c3c88bc5fda35529fd410ea4
gi-ong <pugo9165@gmail.com>
2022-02-07 18:40:48 +0900

    "setting config"

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b#s*_o(3t3ai_k(c5po@h7a=nj5#vjkd3u7ckhnx@)mi=8fn67'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['airbnbnew.eba-ewscepnv.ap-northeast-2.elasticbeanstalk.com']
 
@@ -91,12 +91,26 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "HOST": "airbnb-new.cj7v0drqubgf.ap-northeast-2.rds.amazonaws.com",
+            "NAME": "airbnb-new",
+            "USER": "postgres",
+            "PASSWORD": "1234qwer",
+            "PORT": "5432",
+        }
+    }
 
 
 # Password validation
